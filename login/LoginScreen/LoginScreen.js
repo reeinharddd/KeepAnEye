89cd13dcbf78e48699c2cca24743cba9d809bf38
Erik <beltraberik@gmail.com>
2024-06-28 01:08:27 -0700

    Logn Screen idea

---
+++ login/LoginScreen/LoginScreen.js
@@ -0,0 +1,34 @@
+/** @format */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginBtn = document.getElementById("login");
+  const signupBtn = document.getElementById("signup");
+
+  if (loginBtn && signupBtn) {
+    loginBtn.addEventListener("click", (e) => {
+      let parent = e.target.parentNode.parentNode;
+      Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+        if (element !== "slide-up") {
+          parent.classList.add("slide-up");
+        } else {
+          signupBtn.parentNode.classList.add("slide-up");
+          parent.classList.remove("slide-up");
+        }
+      });
+    });
+
+    signupBtn.addEventListener("click", (e) => {
+      let parent = e.target.parentNode;
+      Array.from(e.target.parentNode.classList).find((element) => {
+        if (element !== "slide-up") {
+          parent.classList.add("slide-up");
+        } else {
+          loginBtn.parentNode.parentNode.classList.add("slide-up");
+          parent.classList.remove("slide-up");
+        }
+      });
+    });
+  } else {
+    console.error("Login or Signup button not found in the DOM.");
+  }
+});
