cccd05cd640bfa0e405cd7014157cb42246ad579
Erik <beltraberik@gmail.com>
2024-07-25 09:39:36 -0700

    Mapa funcional 1

--- dashboard/UserInfo/GeneralInfo.js
+++ dashboard/UserInfo/GeneralInfo.js
@@ -10,12 +10,11 @@ let geofenceCircle;
 
 // Función para inicializar el mapa
 function initMap() {
-  const mapCenter = { lat: 0, lng: 0 }; // Centro inicial del mapa
-  map = L.map("map").setView([mapCenter.lat, mapCenter.lng], 10); // Inicializar mapa con Leaflet
+  const mapCenter = { lat: 34.052235, lng: -118.243683 }; // Centro inicial del mapa cerca de Los Ángeles
+  map = L.map("map").setView([mapCenter.lat, mapCenter.lng], 25); // Inicializar mapa con Leaflet, nivel de zoom 13
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 18,
-    zoom: 10000,
+    maxZoom: 25,
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
@@ -23,7 +22,6 @@ function initMap() {
   // Llamar a la función para obtener y mostrar las métricas del usuario
   fetchUserMetrics();
 }
-// Función para obtener el perfil del usuario
 // Función para obtener las métricas del usuario
 async function fetchUserMetrics() {
   const token = localStorage.getItem("token");
@@ -58,6 +56,12 @@ async function fetchUserMetrics() {
   }
 }
 function updateMap(metrics) {
+  // Si no hay métricas, no hacer nada
+  if (metrics.length === 0) {
+    console.warn("No metrics available to display");
+    return;
+  }
+
   // Encontrar la ubicación más reciente
   const latestLocation = metrics.reduce((prev, current) => {
     return new Date(current.timestamp) > new Date(prev.timestamp)
@@ -84,10 +88,10 @@ function updateMap(metrics) {
   // Centrar el mapa en la ubicación más reciente
   map.setView(
     [latestLocation.coordinates.latitude, latestLocation.coordinates.longitude],
-    10
+    13 // Nivel de zoom, ajusta según sea necesario
   );
 
-  // Definir un círculo para el geofence (ejemplo: radio de 5 km)
+  // Definir un círculo para el geofence
   if (geofenceCircle) {
     geofenceCircle.setLatLng([
       latestLocation.coordinates.latitude,
@@ -100,9 +104,9 @@ function updateMap(metrics) {
         latestLocation.coordinates.longitude,
       ],
       {
-        color: "red",
-        fillColor: "#f03",
-        fillOpacity: 0.5,
+        color: "#00A696", // Color del borde del círculo
+        fillColor: "#00A696", // Color de relleno del círculo
+        fillOpacity: 0.3, // Opacidad de relleno
         radius: 5000, // Radio de 5 km (ajusta según sea necesario)
       }
     ).addTo(map);
@@ -216,20 +220,20 @@ async function fetchUserProfile() {
     let userId;
     if (data.user_type === "admin") {
       if (data.patients && data.patients.length > 0) {
-        userId = data.patients[0].patient_id;
+        userId = data.patients[0].patientId;
       } else {
         console.error("No valid userId found for admin user with no patients");
         redirectToLogin();
         return;
       }
     } else {
-      userId = data._id;
+      userId = data.id;
     }
 
     localStorage.setItem("userId", userId);
 
-    const fullName = `${data.name.first_name} ${data.name.last_name}`;
-    const userPhoto = data.user_photo;
+    const fullName = `${data.name.firstName} ${data.name.lastName}`;
+    const userPhoto = data.userPhoto;
 
     // Actualizar elementos DOM con los datos del usuario
     const welcomeTitle = document.querySelector(
