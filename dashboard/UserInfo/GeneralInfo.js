b7648dc1dd2f2695cb9b96beb804a9e91feeba91
Erik <beltraberik@gmail.com>
2024-07-25 10:12:28 -0700

    Sistema para mapa, y desplegar los signos vitales, LISTO

--- dashboard/UserInfo/GeneralInfo.js
+++ dashboard/UserInfo/GeneralInfo.js
@@ -8,185 +8,11 @@ let map;
 let pinMarker;
 let geofenceCircle;
 
-// Función para inicializar el mapa
-function initMap() {
-  const mapCenter = { lat: 34.052235, lng: -118.243683 }; // Centro inicial del mapa cerca de Los Ángeles
-  map = L.map("map").setView([mapCenter.lat, mapCenter.lng], 25); // Inicializar mapa con Leaflet, nivel de zoom 13
-
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 25,
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
-
-  // Llamar a la función para obtener y mostrar las métricas del usuario
-  fetchUserMetrics();
-}
-// Función para obtener las métricas del usuario
-async function fetchUserMetrics() {
-  const token = localStorage.getItem("token");
-  const userId = localStorage.getItem("userId");
-  console.log("userId", userId);
-  if (!token || !userId) {
-    console.error("Token or User ID not found");
-    redirectToLogin();
-    return;
-  }
-
-  try {
-    const response = await fetch(`${apiUrl}/metrics/location/${userId}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch metrics: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    console.log("User metrics:", data);
-
-    // Llamar a la función para renderizar las métricas en el gráfico
-    updateMap(data);
-  } catch (error) {
-    console.error("Error fetching user metrics:", error.message);
-  }
-}
-function updateMap(metrics) {
-  // Si no hay métricas, no hacer nada
-  if (metrics.length === 0) {
-    console.warn("No metrics available to display");
-    return;
-  }
-
-  // Encontrar la ubicación más reciente
-  const latestLocation = metrics.reduce((prev, current) => {
-    return new Date(current.timestamp) > new Date(prev.timestamp)
-      ? current
-      : prev;
-  });
-
-  // Si el marcador ya existe, actualizar su posición; de lo contrario, crear uno nuevo
-  if (pinMarker) {
-    pinMarker.setLatLng([
-      latestLocation.coordinates.latitude,
-      latestLocation.coordinates.longitude,
-    ]);
-  } else {
-    pinMarker = L.marker([
-      latestLocation.coordinates.latitude,
-      latestLocation.coordinates.longitude,
-    ])
-      .addTo(map)
-      .bindPopup("Última ubicación")
-      .openPopup();
-  }
-
-  // Centrar el mapa en la ubicación más reciente
-  map.setView(
-    [latestLocation.coordinates.latitude, latestLocation.coordinates.longitude],
-    13 // Nivel de zoom, ajusta según sea necesario
-  );
-
-  // Definir un círculo para el geofence
-  if (geofenceCircle) {
-    geofenceCircle.setLatLng([
-      latestLocation.coordinates.latitude,
-      latestLocation.coordinates.longitude,
-    ]);
-  } else {
-    geofenceCircle = L.circle(
-      [
-        latestLocation.coordinates.latitude,
-        latestLocation.coordinates.longitude,
-      ],
-      {
-        color: "#00A696", // Color del borde del círculo
-        fillColor: "#00A696", // Color de relleno del círculo
-        fillOpacity: 0.3, // Opacidad de relleno
-        radius: 5000, // Radio de 5 km (ajusta según sea necesario)
-      }
-    ).addTo(map);
-  }
-}
+//Funcion para obtener los datos del usuario cunado se cargue la pagina
+window.onload = fetchUserProfile;
 
 // Llamar a la función de inicialización del mapa cuando se cargue la página
 document.addEventListener("DOMContentLoaded", initMap);
-
-// Función para obtener los documentos del usuario
-async function fetchUserMedicalInfo() {
-  const token = localStorage.getItem("token");
-  const userId = localStorage.getItem("userId");
-
-  if (!token || !userId) {
-    console.error("Token or User ID not found");
-    redirectToLogin();
-    return;
-  }
-
-  try {
-    const response = await fetch(`${apiUrl}/medical-info/${userId}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    if (!response.ok) {
-      if (response.status === 401) {
-        console.error("Unauthorized access - redirecting to login");
-        redirectToLogin();
-      }
-      throw new Error(`Failed to fetch medical info: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    console.log("User medical info:", data);
-    fetchUserMedicalDocuments();
-
-    // Aquí puedes manejar los datos recibidos y actualizar la UI
-  } catch (error) {
-    console.error("Error fetching medical info:", error.message);
-    // Añade manejo adicional de errores según sea necesario
-  }
-}
-
-//Obtener todos los documentos de un usurio
-
-async function fetchUserMedicalDocuments() {
-  const token = localStorage.getItem("token");
-  const userId = localStorage.getItem("userId");
-  if (!token || !userId) {
-    console.error("Token or User ID not found");
-    redirectToLogin();
-    return;
-  }
-
-  try {
-    const response = await fetch(`${apiUrl}/medical-info/documents/${userId}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(
-        `Failed to fetch medical documents: ${response.statusText}`
-      );
-    }
-
-    const data = await response.json();
-    console.log("User medical documents:", data);
-  } catch (error) {
-    console.error("Error fetching medical documents:", error.message);
-  }
-}
 
 // Llamada a la función principal de obtención de perfil de usuario y datos iniciales
 async function fetchUserProfile() {
@@ -261,15 +87,273 @@ async function fetchUserProfile() {
     }
 
     // Llamar a las funciones de obtención de métricas y documentos después de obtener el perfil
-    fetchUserMetrics();
+    fetchUserLocation();
     fetchUserMedicalInfo();
+    fetchAllUserMetrics();
   } catch (error) {
     console.error("Error fetching user profile:", error.message);
     redirectToLogin();
   }
 }
 
-window.onload = fetchUserProfile;
+//FUncion para obtener todas las metrics
+async function fetchAllUserMetrics() {
+  const token = localStorage.getItem("token");
+  const userId = localStorage.getItem("userId");
+
+  if (!token || !userId) {
+    console.error("Token or User ID not found");
+    redirectToLogin(); // Función para redirigir al usuario a la página de inicio de sesión
+    return;
+  }
+
+  try {
+    const response = await fetch(`${apiUrl}/metrics/all/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch metrics: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log("All user metrics:", data);
+
+    if (data.length === 0) {
+      console.warn("No metrics available to display");
+      return;
+    }
+
+    // Extraer el último resultado de cada métrica
+    const latestMetrics = {
+      heartRate: null,
+      temperature: null,
+    };
+
+    data.forEach((record) => {
+      if (record.heartRate.length > 0) {
+        const latestHeartRate = record.heartRate.reduce((prev, current) =>
+          new Date(current.timestamp) > new Date(prev.timestamp)
+            ? current
+            : prev
+        );
+        latestMetrics.heartRate = latestHeartRate;
+      }
+
+      if (record.temperature.length > 0) {
+        const latestTemperature = record.temperature.reduce((prev, current) =>
+          new Date(current.timestamp) > new Date(prev.timestamp)
+            ? current
+            : prev
+        );
+        latestMetrics.temperature = latestTemperature;
+      }
+    });
+
+    // Inyectar los últimos resultados en el DOM
+    const heartRateTotalElement = document.getElementById("heartRateTotal");
+    const temperatureTotalElement = document.getElementById("temperatureTotal");
+
+    if (heartRateTotalElement) {
+      heartRateTotalElement.textContent = latestMetrics.heartRate
+        ? `${latestMetrics.heartRate.value} bpm`
+        : "No data";
+    }
+
+    if (temperatureTotalElement) {
+      temperatureTotalElement.textContent = latestMetrics.temperature
+        ? `${latestMetrics.temperature.value} °C`
+        : "No data";
+    }
+  } catch (error) {
+    console.error("Error fetching all user metrics:", error.message);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", fetchAllUserMetrics);
+
+// Función para obtener la ubicaion del usuario
+async function fetchUserLocation() {
+  const token = localStorage.getItem("token");
+  const userId = localStorage.getItem("userId");
+  console.log("userId", userId);
+  if (!token || !userId) {
+    console.error("Token or User ID not found");
+    redirectToLogin();
+    return;
+  }
+
+  try {
+    const response = await fetch(`${apiUrl}/metrics/location/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch metrics: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log("User Location:", data);
+
+    // Llamar a la función para renderizar las métricas en el gráfico
+    updateMap(data);
+  } catch (error) {
+    console.error("Error fetching user metrics:", error.message);
+  }
+}
+
+// Función para inicializar el mapa
+function initMap() {
+  const mapCenter = { lat: 34.052235, lng: -118.243683 }; // Centro inicial del mapa cerca de Los Ángeles
+  map = L.map("map").setView([mapCenter.lat, mapCenter.lng], 25); // Inicializar mapa con Leaflet, nivel de zoom 13
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 25,
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  // Llamar a la función para obtener y mostrar las métricas del usuario
+  fetchUserLocation();
+}
+
+//Funcion para actualizar el mapa con la ubicación del usuario
+function updateMap(metrics) {
+  // Si no hay métricas, no hacer nada
+  if (metrics.length === 0) {
+    console.warn("No metrics available to display");
+    return;
+  }
+
+  // Encontrar la ubicación más reciente
+  const latestLocation = metrics.reduce((prev, current) => {
+    return new Date(current.timestamp) > new Date(prev.timestamp)
+      ? current
+      : prev;
+  });
+
+  // Si el marcador ya existe, actualizar su posición; de lo contrario, crear uno nuevo
+  if (pinMarker) {
+    pinMarker.setLatLng([
+      latestLocation.coordinates.latitude,
+      latestLocation.coordinates.longitude,
+    ]);
+  } else {
+    pinMarker = L.marker([
+      latestLocation.coordinates.latitude,
+      latestLocation.coordinates.longitude,
+    ])
+      .addTo(map)
+      .bindPopup("Última ubicación")
+      .openPopup();
+  }
+
+  // Centrar el mapa en la ubicación más reciente
+  map.setView(
+    [latestLocation.coordinates.latitude, latestLocation.coordinates.longitude],
+    13 // Nivel de zoom, ajusta según sea necesario
+  );
+
+  // Definir un círculo para el geofence
+  if (geofenceCircle) {
+    geofenceCircle.setLatLng([
+      latestLocation.coordinates.latitude,
+      latestLocation.coordinates.longitude,
+    ]);
+  } else {
+    geofenceCircle = L.circle(
+      [
+        latestLocation.coordinates.latitude,
+        latestLocation.coordinates.longitude,
+      ],
+      {
+        color: "#00A696", // Color del borde del círculo
+        fillColor: "#00A696", // Color de relleno del círculo
+        fillOpacity: 0.3, // Opacidad de relleno
+        radius: 5000, // Radio de 5 km (ajusta según sea necesario)
+      }
+    ).addTo(map);
+  }
+}
+
+// Función para obtener los documentos del usuario
+async function fetchUserMedicalInfo() {
+  const token = localStorage.getItem("token");
+  const userId = localStorage.getItem("userId");
+
+  if (!token || !userId) {
+    console.error("Token or User ID not found");
+    redirectToLogin();
+    return;
+  }
+
+  try {
+    const response = await fetch(`${apiUrl}/medical-info/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      if (response.status === 401) {
+        console.error("Unauthorized access - redirecting to login");
+        redirectToLogin();
+      }
+      throw new Error(`Failed to fetch medical info: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log("User medical info:", data);
+    fetchUserMedicalDocuments();
+
+    // Aquí puedes manejar los datos recibidos y actualizar la UI
+  } catch (error) {
+    console.error("Error fetching medical info:", error.message);
+    // Añade manejo adicional de errores según sea necesario
+  }
+}
+
+//Obtener todos los documentos de un usurio
+async function fetchUserMedicalDocuments() {
+  const token = localStorage.getItem("token");
+  const userId = localStorage.getItem("userId");
+  if (!token || !userId) {
+    console.error("Token or User ID not found");
+    redirectToLogin();
+    return;
+  }
+
+  try {
+    const response = await fetch(`${apiUrl}/medical-info/documents/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch medical documents: ${response.statusText}`
+      );
+    }
+
+    const data = await response.json();
+    console.log("User medical documents:", data);
+  } catch (error) {
+    console.error("Error fetching medical documents:", error.message);
+  }
+}
 
 // function redirectToLogin() {
 //   window.location.href = "../../login/LoginScreen/LoginScreen.html";
